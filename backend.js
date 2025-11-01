c04b67c61d7f46e0078d1d21c4e5c6492e2456a2
Zergling25 <mharrie@oakland.edu>
2025-11-01 13:13:05 -0400

    Refactor songs array to be module-scoped

--- backend.js
+++ backend.js
@@ -1,14 +1,15 @@
+const songs = [
+    "Can't Hold Us Macklemore",
+    "GTA 2 Rarin",
+    "Assumptions Sam Gellaitry",
+    "Levitating Dua Lipa",
+    "Loyal Odesza",
+    "How Long Charlie Puth",
+    "Hyperspace Sam I"
+];
+
 function requestSong() {
-    const songs = [
-        "Can't Hold Us Macklemore",
-        "GTA 2 Rarin",
-        "Assumptions Sam Gellaitry",
-        "Levitating Dua Lipa",
-        "Loyal Odesza",
-        "How Long Charlie Puth",
-        "Hyperspace Sam I"
-    ]
     return songs[Math.floor(Math.random() * songs.length)];
 }
 
-export { requestSong };
+export { requestSong, songs };
